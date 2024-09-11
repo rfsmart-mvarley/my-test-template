499250b2165633ac59cef372751cbb33469283ed
Matt Varley <matthew.varley@rfsmart.com>
2024-09-11 11:50:51 +0100

    Bootstrap updates

--- bootstrap.ps1
+++ bootstrap.ps1
@@ -1,5 +1,4 @@
 $folderName = Split-Path -Path (Get-Location) -Leaf
-$folderName = "phoenix-something-api"
 if(-not ($folderName -match "phoenix-[\w]*-api")) {
     Write-Host "ERR: Repo name should conform to phoenix-{apiName}-api syntax" -BackgroundColor Red
     exit 1
@@ -7,51 +6,49 @@ if(-not ($folderName -match "phoenix-[\w]*-api")) {
 
 $TextInfo = (Get-Culture).TextInfo
 $apiname = $TextInfo.ToTitleCase(($folderName -Split '-')[1])
-# $apiName = "TEMPLATE"
-$template = "TEMPLATE"
+$templateTitleCase = "TEMPLATE"
+$templateLowerCase = "L-TEMPLATE"
 
 ForEach ($File in (Get-ChildItem -Recurse -File -Exclude *.ps1)) {
     if ($File.Directory -match 'obj') {
         continue
     }
 
-    if ($File.Name -eq "docker-compose.yml") {
-        (Get-Content $File) -Replace $template,$apiName.ToLower() |
-            Set-Content $File
-    }
-    else {
-        (Get-Content $File) -Replace $template,$apiName |
+    $content = (Get-Content $File)
+
+    if ($content -match $templateLowerCase || $content -match $templateTitleCase) {
+        $content -Replace $templateLowerCase,$apiName.ToLower() `
+            -Replace $templateTitleCase,$apiName |
             Set-Content $File
     }
 
-    if ($File.Name -match $template) {
+    if ($File.Name -match $templateTitleCase) {
         Write-Host "Renaming file: " + $File.Name
         Rename-Item -Path $File.FullName -NewName ($File.Name -replace $template,$apiName)
     }
+
+    if ($File.Name -match $templateLowerCase) {
+        Write-Host "Renaming file: " + $File.Name
+        Rename-Item -Path $File.FullName -NewName ($File.Name -replace $template,$apiName.ToLower())
+    }
 }
 
-ForEach ($File in (Get-ChildItem -Directory)) {
-    if ($File.Name -match $template) {
-        Write-Host "Renaming directory: " + $File.Name
-        Rename-Item -Path $File.FullName -NewName ($File.Name -replace $template,$apiName)
+ForEach ($Dir in (Get-ChildItem -Directory -Recurse)) {
+    if ($Dir.Name -match $templateTitleCase) {
+        Write-Host "Renaming directory: " + $Dir.Name
+        Rename-Item -Path $Dir.FullName -NewName ($Dir.Name -replace $template,$apiName)
+    }
+    
+    if ($Dir.Name -match $templateLowerCase) {
+        Write-Host "Renaming directory: " + $Dir.Name
+        Rename-Item -Path $Dir.FullName -NewName ($Dir.Name -replace $template,$apiName.ToLower())
     }
 }
 
 # update listener_rule_priority - needs to be different for each API
-
-# # create projects
-# dotnet new webapi --name "Rfsmart.Phoenix.$apiName.Web" --use-controllers
-# dotnet new classlib --name "Rfsmart.Phoenix.$apiName" 
-# dotnet new nunit --name "Rfsmart.Phoenix.$apiName.Tests"
-
-# # add classlib ref to web project
-# dotnet add "Rfsmart.Phoenix.$apiName.Web/Rfsmart.Phoenix.$apiName.Web.csproj" reference "Rfsmart.Phoenix.$apiName/Rfsmart.Phoenix.$apiName.csproj"
-
-# # add classlib ref to test project
-# dotnet add "Rfsmart.Phoenix.$apiName.Tests/Rfsmart.Phoenix.$apiName.Tests.csproj" reference "Rfsmart.Phoenix.$apiName/Rfsmart.Phoenix.$apiName.csproj"
-
-# # create sln
-# dotnet new sln --name "Rfsmart.Phoenix.$apiName.Api"
-# dotnet sln add "Rfsmart.Phoenix.$apiName.Web"
-# dotnet sln add "Rfsmart.Phoenix.$apiName"
-# dotnet sln add "Rfsmart.Phoenix.$apiName.Tests"
+Write-Host "Listener rule priority must be different for each API"
+Write-Host "What should the new listener rule priority be?"
+$lrp = Read-Host
+$mainTf = "./iac/deploys/$apiName-web/main.tf"
+(Get-Content $mainTf) -Replace "listener_rule_priority = 4","listener_rule_priority = $lrp" |
+    Set-Content $mainTf
